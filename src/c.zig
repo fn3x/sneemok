c0627ce633ca5aabb467bc0232d5b0985112b1ca
Art P. <fn3x@yandex.ru>
2025-11-14 05:19:30 +0400

    fix: define STB_IMAGE_IMPLEMENTATION

--- src/c.zig
+++ src/c.zig
@@ -1,5 +1,6 @@
 pub const c = @cImport({
     @cDefine("STB_IMAGE_IMPLEMENTATION", "");
+    @cDefine("STB_IMAGE_IMPLEMENTATION", "");
     @cDefine("STBI_ONLY_PNG", "1");
     @cDefine("STBI_NO_SIMD", "1");
     @cInclude("stb_image.h");
